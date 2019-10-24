e203c74204880a76c251d032eb9ca88dd63a7682
Kennard Ng <Kennard_ng@Outlook.com>
2019-10-24 09:04:38 +0800

    make insert sotring  in place

--- src/cola/cola.py
+++ src/cola/cola.py
@@ -32,51 +32,45 @@ class NaiveCoa:
         return idx
 
     def insert(self, item):
-        if (self.n_items + 1) >= self.size:
-            raise BufferError('size is too small')
-        self.n_items += 1  # increment counter
+        self.n_items += 1
+        if self.n_items >= self.size:
+            raise BufferError('too much shit')
 
         start_idx = 0
-        items_to_insert = [item]
+        array_size = 2
+        n_insertions = 1
+        prev_array = np.array([item])
         for i in range(self.n_arrays):
-            end_idx = 2**(i+1)
-            subarray = self.array[start_idx:end_idx]
-            has_items = self.has_item[start_idx:end_idx]
+            current_arr = self.array[start_idx:start_idx + array_size]
+            n_array_item = self.n_array_items[i]
 
-            if np.all(has_items):  # array is full, merge the arrays
-                n_insertions = len(items_to_insert)
-                n_new_insertions = len(has_items)
-                merged_array = list()
-                idx, new_idx = 0, 0
-                while (idx < n_insertions) and (new_idx < n_new_insertions):
-                    item = items_to_insert[idx]
-                    new_item = subarray[new_idx]
-                    if item <= new_item:
-                        merged_array.append(item)
+            if [n_insertions + n_array_item] >= (array_size >> 1):  # if it will fill up both arrays
+                insert_idx = start_idx + n_array_item - 1
+
+                # do a merge here, inserting the largest item first
+                i1 = n_array_item - 1
+                i2 = n_insertions - 1
+                while i1 >= 0 and i2 >= 0:
+                    if current_arr[i1] > prev_array[i2]:
+                        current_arr[insert_idx] = current_arr[i1]
+                        i1 -= 1
                     else:
-                        merged_array.append(new_item)
+                        current_arr[insert_idx] = prev_array[i2]
+                        i2 -= 1
+                    insert_idx -= 1
 
-                if idx < n_insertions:
-                    merged_array += items_to_insert[idx:]
-                    assert new_idx == n_new_insertions
-                elif new_idx < n_new_insertions:
-                    merged_array += subarray[new_idx:]
-                    assert idx == n_insertions
-
-                items_to_insert = merged_array
-                self.has_item &= False  # bitmasking
-            else:  # array has space, so we can insert
-                insert_idx = 0
-                for j, has_item in enumerate(self.has_item):
-                    if not has_item:
-                        insert_idx = j
-                        break
-                n_insertions = len(items_to_insert)
-                final_idx = insert_idx + n_insertions
-                self.array[insert_idx:final_idx] = np.array(items_to_insert)
-                self.has_item[insert_idx:final_idx] |= True
+                if i1 >= 0:
+                    current_arr[:insert_idx + 1] = current_arr[:i1 + 1]
+                else:
+                    current_arr[:insert_idx + 1] = prev_array[:i1 + 1]
+                n_insertions += n_array_item
+                self.n_array_items[i] = 0
+            else:  # simple
+                self.array[start_idx:start_idx + n_array_item] = prev_array[0:n_insertions]
+                self.n_array_items[i] += n_insertions
                 break
-            start_idx = end_idx
+            start_idx += array_size
+            array_size = array_size << 1  # multiply by 2
 
 
 class Cola:
@@ -87,28 +81,12 @@ class Cola:
         self.size = size
         self.n_arrays = math.ceil(math.log(self.size, base=2)) + 1
         self.array = np.empty(2 ** (self.n_arrays + 1), dtype=int)
+        self.n_array_items = np.zeros(shape=self.n_arrays, dtype=int)
         self.has_item = np.zeros_like(self.array, dtype=bool)
         self.n_items = 0
 
     def search(self, item):
         raise NotImplementedError
-
-    @staticmethod
-    def get_insert_idx(array, length):
-        ones_idx = 0
-        zeros_idx = length - 1
-        if array[ones_idx] == 0:
-            return 0
-
-        while True:
-            mid_idx = (ones_idx + zeros_idx) // 2
-            if mid_idx:
-                ones_idx = mid_idx
-            else:
-                zeros_idx = mid_idx
-
-            if ones_idx ==
-
 
     def insert(self, item):
         self.n_items += 1
@@ -117,12 +95,39 @@ class Cola:
 
         start_idx = 0
         array_size = 2
+        n_insertions = 1
+        prev_array = np.array([item])
         for i in range(self.n_arrays):
-            array_size = array_size << 2
-            insert_idx = np.argwhere(self.has_item[start_idx:start_idx+(array_size >> 2)])[-1]
-            if insert_idx >= :
-                co
+            current_arr = self.array[start_idx:start_idx + array_size]
+            n_array_item = self.n_array_items[i]
+
+            if [n_insertions + n_array_item] >= (array_size >> 1):  # if it will fill up both arrays
+                insert_idx = start_idx + n_array_item - 1
+
+                # do a merge here, inserting the largest item first
+                i1 = n_array_item - 1
+                i2 = n_insertions - 1
+                while i1 >= 0 and i2 >= 0:
+                    if current_arr[i1] > prev_array[i2]:
+                        current_arr[insert_idx] = current_arr[i1]
+                        i1 -= 1
+                    else:
+                        current_arr[insert_idx] = prev_array[i2]
+                        i2 -= 1
+                    insert_idx -= 1
+
+                if i1 >= 0:
+                    current_arr[:insert_idx+1] = current_arr[:i1+1]
+                else:
+                    current_arr[:insert_idx+1] = prev_array[:i1+1]
+                n_insertions += n_array_item
+                self.n_array_items[i] = 0
+            else:  # simple
+                self.array[start_idx:start_idx+n_array_item] = prev_array[0:n_insertions]
+                self.n_array_items[i] += n_insertions
+                break
             start_idx += array_size
+            array_size = array_size << 1  # multiply by 2
 
 
 def main():
