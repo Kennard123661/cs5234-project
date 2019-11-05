48342537dc360d94fea201e7744d3ef40c8c5d6e
kennard123661 <kennard_ng@outlook.com>
2019-11-05 21:49:49 +0800

    adding fractional cola insert

--- src/data_structures/fractional_cola.py
+++ src/data_structures/fractional_cola.py
@@ -9,7 +9,7 @@ from data_structures.base import WriteOptimizedDS
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 storage_dir = os.path.join(base_dir, 'storage')
 
-
+N_ARRAY_PER_LEVEL = 2
 REAL_POINTER_STRIDE = 8
 VIRTUAL_POINTER_STRIDE = 4
 
@@ -26,7 +26,7 @@ class FractionalCola(WriteOptimizedDS):
         self.disk_size = self.block_size  # reading and writing will be in blocks.
         for i in range(self.n_levels):
             array_size = self.growth_factor**i
-            self.disk_size += (array_size << 1)
+            self.disk_size += N_ARRAY_PER_LEVEL * array_size
         assert self.mem_size < self.disk_size
 
         # todo: transfer this over to the base.py
@@ -37,30 +37,17 @@ class FractionalCola(WriteOptimizedDS):
             if not os.path.exists(dirname):
                 os.makedirs(dirname)
 
-        self.n_level_virtual_pointers = np.zeros(shape=self.n_levels, dtype=np.int)
+        self.level_n_virtual_pointers = np.zeros(shape=self.n_levels, dtype=int)
         for i in range(self.n_levels):
-            level_size = 2**i
-            self.n_level_virtual_pointers[i] = level_size // VIRTUAL_POINTER_STRIDE
-        self.n_virtual_pointers = np.sum(self.n_level_virtual_pointers) + self.block_size
-        self.cache_n_virtual_pointers = 0
-
-        start_idx = 0
-        level_size = 2
-        for i in range(self.n_levels):
-            end_idx = start_idx + level_size
-            if end_idx < self.mem_size:
-                self.cache_n_virtual_pointers += self.n_level_virtual_pointers[i]
-            else:
-                self.cache_n_virtual_pointers += (min(end_idx, self.mem_size) - start_idx) // 4
-            start_idx = end_idx
-            level_size = level_size << 1
+            array_size = 2**i
+            self.level_n_virtual_pointers[i] = array_size // 4
 
         disk = h5py.File(self.disk_filepath, 'w')
         disk.create_dataset('dataset', shape=(self.disk_size, ), dtype='i8')
         disk.create_dataset('is_pointer', shape=(self.disk_size, ), dtype='i8')
         disk.create_dataset('real_ref', shape=(self.disk_size, ), dtype='i8')
-        disk.create_dataset('virtual_left', shape=(self.n_virtual_pointers, ), dtype='i8')
-        disk.create_dataset('virtual_right', shape=(self.n_virtual_pointers, ), dtype='i8')
+        disk.create_dataset('virtual_left', shape=(self.disk_size, ), dtype='i8')
+        disk.create_dataset('virtual_right', shape=(self.disk_size, ), dtype='i8')
         disk.close()
 
         self.disk = h5py.File(self.disk_filepath, 'r+')
@@ -71,19 +58,16 @@ class FractionalCola(WriteOptimizedDS):
         self.disk_r_points = self.disk['real_ref']
 
         self.cache_data = np.zeros(shape=self.mem_size, dtype=np.int).tolist()
-        self.cache_v_lefts = list(-np.ones(shape=self.cache_n_virtual_pointers, dtype=np.int))
-        self.cache_v_rights = list(-np.ones(shape=self.cache_n_virtual_pointers, dtype=np.int))
         self.cache_is_pointers = list(np.zeros(shape=self.mem_size, dtype=bool))
         self.cache_r_points = list(np.zeros(shape=self.mem_size, dtype=np.int))
 
+        self.level_n_pointers = np.zeros(shape=self.n_levels, dtype=np.int)
         self.n_level_items = np.zeros(shape=self.n_levels, dtype=np.int)
         self.n_items = 0
 
     def __del__(self):
         """ save the cache data into the disk and close the disk. """
         self.write_disk(self.disk_data, 0, self.mem_size, self.cache_data)
-        self.write_disk(self.disk_v_lefts, 0, self.cache_n_virtual_pointers, self.cache_v_lefts)
-        self.write_disk(self.disk_v_rights, 0, self.cache_n_virtual_pointers, self.cache_v_rights)
         self.write_disk(self.disk_is_pointers, 0, self.mem_size, self.cache_is_pointers)
         self.write_disk(self.disk_r_points, 0, self.mem_size, self.cache_r_points)
         self.disk.close()
@@ -95,71 +79,141 @@ class FractionalCola(WriteOptimizedDS):
         n_insertions = 1
         insert_arr = [item]
 
-        insert_level = 0
         start_idx = 0
         loaded_arr = copy.deepcopy(self.cache_data)  # ensure no changes to the cache array.
-        loaded_v_lefts = copy.deepcopy(self.cache_v_lefts)  # ensure no changes to the cache array.
-        loaded_v_rights = copy.deepcopy(self.cache_v_rights)  # ensure no changes to the cache array.
-        loaded_is_pointers = copy.deepcopy(self.cache_is_pointers)  # ensure no changes to the cache array.
-        loaded_r_points = copy.deepcopy(self.cache_r_points)  # ensure no changes to the cache array.
+        loaded_is_pointers = copy.deepcopy(self.cache_is_pointers)
+        loaded_r_points = copy.deepcopy(self.cache_r_points)
         loaded_arr_start_idx = 0
         loaded_arr_end_idx = self.mem_size
 
+
+        last_insert_level = 0
+        last_insert_arr = None
         for i in range(self.n_levels):
-            level_size = array_size << 1
+            level_size = array_size * N_ARRAY_PER_LEVEL
             end_idx = start_idx + level_size
 
             # load as much as we need based on blocks.
             while loaded_arr_end_idx < end_idx:
-                loaded_arr += self.read_disk_block(loaded_arr_end_idx)
+                loaded_arr += self.read_disk_block(self.disk_data, loaded_arr_end_idx)
+                loaded_is_pointers += self.read_disk_block(self.disk_is_pointers, loaded_arr_end_idx)
+                loaded_r_points += self.read_disk_block(self.disk_r_points, loaded_arr_end_idx)
                 loaded_arr_end_idx += self.block_size
 
             # update the loaded array
             if start_idx > loaded_arr_start_idx:
                 loaded_arr = loaded_arr[start_idx-loaded_arr_start_idx:]
+                loaded_is_pointers = loaded_is_pointers[start_idx-loaded_arr_start_idx:]
+                loaded_r_points = loaded_r_points[start_idx-loaded_arr_start_idx:]
                 loaded_arr_start_idx = start_idx
 
-            n_level_items = self.n_level_items[i]
+            level_n_items = self.n_level_items[i]
             current_arr = loaded_arr[:end_idx-start_idx]
+            curr_is_pointers = loaded_is_pointers[:end_idx-start_idx]
+            curr_r_points = loaded_r_points[:end_idx-start_idx]
 
             # perform the merge into the current array, we merge from the back.
-            insert_idx = n_level_items + n_insertions - 1
-            i1 = n_level_items - 1
+            insert_idx = level_n_items + n_insertions - 1
+            i1 = level_n_items - 1
             i2 = n_insertions - 1
             while i1 >= 0 and i2 >= 0:
                 if current_arr[i1] > insert_arr[i2]:
                     current_arr[insert_idx] = current_arr[i1]
+                    curr_is_pointers[insert_idx] = curr_is_pointers[i1]
+                    curr_r_points[insert_idx] = curr_r_points[i1]
                     i1 -= 1
                 else:
                     current_arr[insert_idx] = insert_arr[i2]
+                    curr_is_pointers[insert_idx] = False
+                    curr_r_points[insert_idx] = -1
                     i2 -= 1
                 insert_idx -= 1
 
             if i1 >= 0:
-                current_arr[:insert_idx + 1] = current_arr[:i1 + 1]
+                current_arr[:insert_idx + 1] = current_arr[:i1+1]
+                curr_is_pointers[:insert_idx+1] = curr_is_pointers[:i1+1]
+                curr_r_points[:insert_idx+1] = curr_r_points[:i1+1]
                 assert i2 < 0
             else:
                 current_arr[:insert_idx + 1] = insert_arr[:i2 + 1]
+                curr_is_pointers[:insert_idx+1] = np.zeros(shape=i2, dtype=bool)
+                curr_r_points[:insert_idx+1] = -np.ones(shape=i2, dtype=int)
                 assert i1 < 0
             # end of merge
 
-            if (n_insertions + n_level_items) > array_size:  # recurse to the next line for insertion
-                insert_arr = current_arr
-                n_insertions += n_level_items
+            if (n_insertions + level_n_items) > array_size:  # recurse to the next line for insertion
+                insert_arr = list()
+                for j, is_pointer in enumerate(curr_is_pointers):
+                    if not is_pointer:
+                        insert_arr.append(current_arr[j])
+                n_insertions = len(insert_arr)
                 self.n_level_items[i] = 0
             else:  # there is enough space to insert all of the data here.
                 self.n_level_items[i] += n_insertions
                 if start_idx >= self.mem_size and end_idx >= self.mem_size:  # both are larger, copy to disk
-                    self.write_disk(start_idx, end_idx, current_arr)
+                    self.write_disk(self.disk_data, start_idx, end_idx, current_arr)
+                    self.write_disk(self.disk_is_pointers, start_idx, end_idx, curr_is_pointers)
+                    self.write_disk(self.disk_r_points, start_idx, end_idx, curr_r_points)
                 elif start_idx < self.mem_size and end_idx < self.mem_size:  # both are smaller, copy to cache
                     self.cache_data[start_idx:end_idx] = current_arr
+                    self.cache_is_pointers[start_idx:end_idx] = curr_is_pointers
+                    self.cache_r_points[start_idx:end_idx] = curr_r_points
                 elif start_idx < self.mem_size:  # copy some to cache and some to the disk.
                     self.cache_data[start_idx:self.mem_size] = current_arr[0:self.mem_size - start_idx]
-                    self.write_disk(self.mem_size, end_idx, current_arr[self.mem_size-start_idx:])
+                    self.cache_is_pointers[start_idx:self.mem_size] = curr_is_pointers[0:self.mem_size - start_idx]
+                    self.cache_r_points[start_idx:self.mem_size] = curr_r_points[0:self.mem_size - start_idx]
+
+                    # copy the remainder to the disk.
+                    self.write_disk(self.disk_data, self.mem_size, end_idx, current_arr[self.mem_size-start_idx:])
+                    self.write_disk(self.disk_is_pointers, self.mem_size, end_idx,
+                                    curr_is_pointers[self.mem_size-start_idx:])
+                    self.write_disk(self.disk_r_points, self.mem_size, end_idx, curr_r_points[self.mem_size-start_idx:])
+                last_insert_arr = current_arr
                 break
 
             start_idx += level_size
             array_size *= self.growth_factor
+            last_insert_level += 1
+
+        for i in reversed(range(last_insert_level)):
+            next_level_n_items = self.n_level_items[i+1]
+            if next_level_n_items == 0:
+                continue
+
+            start_idx = 2**i
+
+            insert_r_points = range(start=0, stop=next_level_n_items, step=REAL_POINTER_STRIDE)
+            insert_is_pointers = np.zeros_like(insert_r_points, dtype=bool)
+            n_insertions = len(insert_r_points)
+            insert_arr = list()
+            for j in insert_r_points:
+                insert_arr.append(last_insert_arr[j])
+
+            self.n_level_items[i] = len(insert_arr)
+            end_idx = start_idx + n_insertions
+            if start_idx >= self.mem_size and end_idx >= self.mem_size:  # both are larger, copy to disk
+                self.write_disk(self.disk_data, start_idx, end_idx, insert_arr)
+                self.write_disk(self.disk_is_pointers, start_idx, end_idx, insert_is_pointers)
+                self.write_disk(self.disk_r_points, start_idx, end_idx, insert_r_points)
+            elif start_idx < self.mem_size and end_idx < self.mem_size:  # insert both in the cache
+                self.cache_data[start_idx:end_idx] = insert_arr
+                self.cache_r_points[start_idx:end_idx] = insert_r_points
+                self.cache_is_pointers[start_idx:end_idx] = insert_is_pointers
+            elif start_idx < self.mem_size:  # copy some to cache and some to the disk.
+                self.cache_data[start_idx:self.mem_size] = insert_arr[0:self.mem_size - start_idx]
+                self.cache_is_pointers[start_idx:self.mem_size] = insert_is_pointers[0:self.mem_size - start_idx]
+                self.cache_r_points[start_idx:self.mem_size] = insert_r_points[0:self.mem_size - start_idx]
+
+                # copy the remainder to the disk.
+                self.write_disk(self.disk_data, self.mem_size, end_idx, insert_arr[self.mem_size-start_idx:])
+                self.write_disk(self.disk_is_pointers, self.mem_size, end_idx,
+                                insert_is_pointers[self.mem_size-start_idx:])
+                self.write_disk(self.disk_r_points, self.mem_size, end_idx, insert_r_points[self.mem_size-start_idx:])
+            last_insert_arr = insert_arr
+
+
+
+
 
     def query(self, item):
         array_size = 1
