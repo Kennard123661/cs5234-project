09b86a1d6d3c5fc356ed4a391cce21ae0c452711
kennard123661 <kennard_ng@outlook.com>
2019-11-11 15:10:45 +0800

    fix basic cola for random data

--- data_structures/basic_cola.py
+++ data_structures/basic_cola.py
@@ -71,7 +71,7 @@ class BasicCola(WriteOptimizedDS):
                     merged_data[merged_i] = level_data[level_i]
                     level_i += 1
                 else:
-                    merged_data[merged_i] = insert_data[level_i]
+                    merged_data[merged_i] = insert_data[insert_i]
                     insert_i += 1
                 merged_i += 1
 
